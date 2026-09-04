6b8d586dca7dd345ed4c6644aa8d9fe026ea6c1e
ShengZhuMS6 <shengzhu@microsoft.com>
2026-09-04 17:02:12 +0200

    banner

--- src/web/layout.js
+++ src/web/layout.js
@@ -152,7 +152,7 @@ ${identityBar(ctx)}
   <div class="govuk-phase-banner">
     <p class="govuk-phase-banner__content">
       <strong class="govuk-tag">Alpha</strong>
-      <span>This is a prototype only created by CCoE for SIT. For technical queries and bug reporting, please contact sheng.zhu@defra.gov.uk</span>
+      <span>This is a prototype built by CCoE for SIT  - all live services but mock data. Please send emails to sheng.zhu@defra.gov.uk to report bugs.</span>
     </p>
   </div>
 
